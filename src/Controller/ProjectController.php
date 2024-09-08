3dc28100307bfaf21591892c8c452d09922967ff
Kerkar <kerkar.kevin@gmail.com>
2024-09-08 15:34:28 +0200

    Base template edited, fixtures for users too, team controller created

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,31 +3,30 @@
 namespace App\Controller;
 
 use App\Repository\ProjectRepository;
-use App\Service\SortArray;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Enum\ProjectStatus;
 
-class ProjectController extends AbstractController
+##Todo : Créer les routes suivantes project/ : list,create,edit,delete,detail.
+##Todo : Créer les routes suivantes task/ : create,edit,delete.
+##Todo : Créer les routes suivantes team/ : list/create/edit/delete/detail
+##Todo : Reprendre le template de base pour adapter le titre de la page (Projet pour la site des projets, le nom du projet sur la page detail etc...)
+
+class HomeController extends AbstractController
 {
-    #[Route('/project/{id}', name: 'project_detail')]
-    public function index(int $id, ProjectRepository $projectRepository): Response
+    #[Route('/', name: 'home')]
+    public function index(ProjectRepository $projectRepository): Response
     {
-        $project = $projectRepository->find($id);
-        $tasks = $project->getTasks();
+        // Ajoutez du code de débogage pour afficher les informations sur les classes
+        $userClass = \App\Entity\User::class;
+        $loadedClass = get_class(new \App\Entity\User());
 
-        $tasksByStatus = [
-        ];
-
-        foreach ($tasks as $task) {
-            $tasksByStatus[$task->getStatus()->getLibelle()][] = $task;
-        }
-
-        return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
-            'project' => $project,
-            'tasksByStatus' => $tasksByStatus,
-
+        $projects = $projectRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'titlePage'=> 'Projets',
+            'projects' => $projects,
         ]);
     }
 }
